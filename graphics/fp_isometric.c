62472a68a86ff56ccf9561cc3b664318ff162bcf
Fauna Polaris <max.maxpinheiro@gmail.com>
2023-08-19 13:59:29 -0300

    almost cube

--- graphics/fp_isometric.c
+++ graphics/fp_isometric.c
@@ -6,7 +6,7 @@
 /*   By: fpolaris <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 12:46:26 by fpolaris          #+#    #+#             */
-/*   Updated: 2023/08/16 13:51:57 by fpolaris         ###   ########.fr       */
+/*   Updated: 2023/08/19 09:34:24 by fpolaris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ void	fp_isometric(t_vector *vertx, int quantity)
 	i = 0;
 	while (i < quantity)
 	{
-		new_x = (vertx[i].x * 0.707) + (vertx[i].y * 0.707);
-		new_y = (vertx[i].x * -0.707) + (vertx[i].y * 0.707);
+		new_x = (vertx[i].x - vertx[i].y) * cos(0.5236);
+		new_y = -vertx[i].z + (vertx[i].x + vertx[i].y) * sin(0.5236);
 		vertx[i].x = new_x;
 		vertx[i].y = new_y;
 		i++;
--- graphics/fp_putvertice.c
+++ graphics/fp_putvertice.c
@@ -3,18 +3,12 @@
 void	fp_putvertice(t_mlx *data, t_vector *vertice, int quantity, int color)
 {
 	int	i;
-	double	scale_x;
-	double	scale_y;
-	double	ratio;
 
-	ratio = (double)WIDTH / (double)HEIGHT;
-	scale_x = WIDTH * ratio / (vertice[2].x - vertice[0].x);
-	scale_y = HEIGHT * ratio / (vertice[2].y - vertice[0].y);
 	i = 0;
 	while (i < quantity)
 	{
-		fp_putpixel(data, scale_x * vertice[i].x + (WIDTH / 2),
-					scale_y * vertice[i].y + (HEIGHT / 2),
+		fp_putpixel(data,vertice[i].x + (WIDTH / 2),
+					vertice[i].y + (HEIGHT / 2),
 					color);
 		i++;
 	}
